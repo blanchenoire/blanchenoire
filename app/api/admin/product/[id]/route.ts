8320f5d04c88e2cf6b84b597766a2d7471940f44
sarvesh123githubacc <sarveshgarg197@gmail.com>
2026-03-24 22:52:11 +0530

    ts fix 3

--- app/api/admin/product/[id]/route.ts
+++ app/api/admin/product/[id]/route.ts
@@ -5,7 +5,7 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function PUT(
   req: NextRequest,
-  { params }: { params: { id: string } }
+  { params }: { params: Promise<{ id: string }> }
 ) {
   try {
     const admin = verifyAdmin(req);
